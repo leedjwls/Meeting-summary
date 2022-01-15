f707916468d7002a59eed7b3b87abcd8024fd460
leedjwls <64123081+leedjwls@users.noreply.github.com>
2022-01-16 01:54:09 +0900

    Delete MeetingNotes-Summary-master directory

--- MeetingNotes-Summary-master/client/src/setupProxy.js
+++
@@ -1,9 +0,0 @@
-const {createProxyMiddleware} = require('http-proxy-middleware');
-
-//CORS ERROR when server and client Port number is different
-module.exports = function (app) {
-  app.use(["/speechtoText", '/socket.io', '/result'],createProxyMiddleware({
-    target: "http://localhost:3030",
-    changeOrigin: true,
-}));
-};
